49788274beb284db3658ef772ea3b076b5005c8a
Phantasmysteria <wallacechan58185@gmail.com>
2023-12-01 22:02:42 +0800

    Added initial C++ AoC Utilities

---
+++ AoCUtils.h
@@ -0,0 +1,65 @@
+#ifndef AOCUTIL
+#define AOCUTIL
+
+#define varName(v) (#v)
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <typeinfo>
+#include <unordered_set>
+
+using namespace std;
+
+// Basic Read Lines, modify type whenever
+vector<string> readLines(string filename)
+{
+    ifstream file(filename);
+    string line;
+    vector<string> result;
+
+    while (getline(file, line)) {result.push_back(line);}
+
+    return result;
+}
+
+template <typename T>
+void print1Dcontainer(T input)
+{
+    cout << "\n=======================\n";
+    cout << "Printing " << typeid(input).name() << "\n";
+    cout << varName(input) << ": ";
+    for (auto i : input) {cout << i << " ";}
+    cout << "\n=======================\n";
+}
+
+template <typename T>
+void print2Dcontainer(T input)
+{
+    cout << "\n=======================\n";
+    cout << "Printing " << typeid(input).name() << "\n";
+    cout << varName(input) << ": \n";
+    for (auto i : input) {for (auto j : i) {cout << j << " ";} cout << "\n";} 
+    cout << "\n=======================\n";
+}
+
+template <typename T>
+vector<T> readLinewithComma(string input)
+{
+    istringstream iss(input);
+    string line;
+    vector<T> result;
+
+    while (getline(iss, line, ',')) {result.push_back(line);}
+
+    return result;
+}
+
+
+#endif
