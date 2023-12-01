3d0e96299dd450cdbb82af01948db9b9852198a4
Phantasmysteria <wallacechan58185@gmail.com>
2023-12-01 22:47:12 +0800

    Added Python and C++ solve templates

---
+++ AoC-2023-Templates/AoC-2023-Day-X-Part-X.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('..')
+import AoCUtils
+from typing import List
+
+inputLines = AoCUtils.listLines(day=1, part=1)
+
+def solve(inp):
+    result = []
+
+    return result
+    
+
+if __name__ == '__main__':
+    result = solve(inputLines)
+    print(result)
---
+++ AoC-2023-Templates/AoC-2023-Day-Y-Part-Y.cpp
@@ -0,0 +1,16 @@
+#include "../AoCUtils.h"
+
+vector<string> solve(vector<string> input) {
+
+
+
+    return input;
+}
+
+int main() {
+
+    vector<string> input = readLines("inputD1P1.txt");
+    print1Dcontainer(solve(input));
+    
+    return 0;
+}
