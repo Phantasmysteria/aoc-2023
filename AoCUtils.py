22beec164ba1d6b1ebf2500db7deff9b8ce78261
Phantasmysteria <wallacechan58185@gmail.com>
2023-12-01 22:03:45 +0800

    Added Python Utility listLines comments

--- AoCUtils.py
+++ AoCUtils.py
@@ -1,10 +1,12 @@
 import typing
 import itertools
 
+# Quick way to convert input file into list of lines
 def listLines(day: str, part: str) -> list[str]:
     with open(f"inputD{day}P{part}.txt", "r") as f:
         return [item.rstrip() for item in f.readlines()]
 
+# Quick way to convert input file into list of list of lines separated by delimiter
 def listLinesWithGroup(day: str, part: str, groupSep: str) -> list[str]:
     with open(f"inputD{day}P{part}.txt", "r") as f:
         return [list(group) for key, group in itertools.groupby([item.rstrip() for item in f.readlines()], lambda x: x == groupSep) if not key]
